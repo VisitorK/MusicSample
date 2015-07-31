1fab3fd89e4d92fc688f342d4cea08e58eb42576
bktmkd <tmkd@qq.com>
2015-07-31 18:40:41 +0800

    新建音乐播放器工程
    Signed-off-by: bktmkd <tmkd@qq.com>

---
+++ src/bktmkd/android/music/BktmkdMusicActivity.java
@@ -0,0 +1,13 @@
+package bktmkd.android.music;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BktmkdMusicActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
