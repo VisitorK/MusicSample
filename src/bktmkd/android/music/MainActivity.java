94c994e92218d6b2d39b6be1a1ee93949ec39e33
bktmkd <bktmkd@hotmail.com>
2015-08-03 22:42:11 +0800

    添加service触发音乐播放
    Signed-off-by: bktmkd <bktmkd@hotmail.com>

---
+++ src/bktmkd/android/music/MainActivity.java
@@ -0,0 +1,67 @@
+package bktmkd.android.music;
+
+import android.R.bool;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import bktmkd.android.music.R;
+import bktmkd.android.services.MusicPlyerService;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+	private ImageButton btnplay;
+	private boolean PlayFlag=false;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnplay=(ImageButton)findViewById(R.id.btnplay);
+        btnplay.setOnClickListener(new OnClickListener() {
+       	 Intent intentSV = new Intent(MainActivity.this, MusicPlyerService.class); 
+			public void onClick(View v) {
+			
+			if(PlayFlag)
+			{
+				btnplay.setBackgroundResource(R.drawable.play);
+				stopService(intentSV);
+				
+			}
+			else
+			{
+
+				btnplay.setBackgroundResource(R.drawable.pause);
+			 
+			      startService(intentSV);  
+			
+			}
+			PlayFlag=!PlayFlag;
+			
+				
+			}
+		});
+     
+    }
+    final ServiceConnection con=new ServiceConnection() {
+		
+		public void onServiceDisconnected(ComponentName name) {
+			// TODO Auto-generated method stub
+			   Toast.makeText(MainActivity.this, "MusicServiceActivity onSeviceDisconnected"
+	                    , Toast.LENGTH_SHORT).show();
+		}
+		
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			// TODO Auto-generated method stub
+			 Toast.makeText(MainActivity.this, "MusicServiceActivity onSeviceDisconnected"
+	                    , Toast.LENGTH_SHORT).show();
+		}
+	};
+}
---
+++ src/bktmkd/android/services/MusicPlyerService.java
@@ -0,0 +1,55 @@
+package bktmkd.android.services;
+
+import android.R;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MusicPlyerService extends Service {
+	private static String TAG = "MusicService";
+	private MediaPlayer mPlayer;
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		 mPlayer.start();
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		Log.d(TAG, "CreateService");
+		mPlayer=MediaPlayer.create(getApplicationContext(), bktmkd.android.music.R.raw.aaa);
+	   mPlayer.setLooping(true);
+		super.onCreate();
+		
+	
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		mPlayer.stop();
+		super.onDestroy();
+	}
+	@Override
+	public void onStart(Intent intent, int startId) {
+		// TODO Auto-generated method stub
+		mPlayer.start();
+	
+		super.onStart(intent, startId);
+	}
+
+	@Override
+	public boolean onUnbind(Intent intent) {
+		// TODO Auto-generated method stub
+		mPlayer.stop();
+		return super.onUnbind(intent);
+	}
+
+}
